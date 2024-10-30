ec77de3720465a48ceee743aa35e979350a58a7a
Niklas Pallon <niklaspallon@gmail.com>
2024-10-30 15:58:44 +0100

    La till filter

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,133 @@
 import 'package:flutter/material.dart';
+import 'add_chores_screen.dart';
+import 'package:provider/provider.dart';
+import 'chores_api.dart';
 
 void main() {
-  runApp(const MyApp());
+  MyState state =
+      MyState(); // Skapar en instans av MyState som håller ordning på todo-listan och hanterar API-kommunikationen
+
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) =>
+          state, //Ger instansen MyState till alla underliggande widgets
+      child: Myhome(),
+    ),
+  );
+}
+
+class Myhome extends StatelessWidget {
+  const Myhome({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: MyApp());
+  }
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
+    final myState = Provider.of<MyState>(
+        context); // Hämtar instansen av MyStatt och lyssnar på förändringar så att UI kan uppdateras när förändirngar sker
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text("Michael Scotts ToDo"),
+        centerTitle: true,
+        backgroundColor: Colors.grey,
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 16.0),
+            child: GestureDetector(
+              onTap: () {
+                myState.sortChores();
+              },
+              child: Icon(Icons.sort),
+            ),
+          ),
+        ],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body: myState.loading //Kollar om data laddas från API
+          ? Center(
+              child: CircularProgressIndicator()) //Laddar det, visa en symbol
+          : ListView(
+              //Annars, visa chorelistan
+              children: myState.choresList
+                  .map((chore) => buildChoreItem(context,
+                      chore)) //Skapar en widget för varje chore i listan
+                  .toList(), //Gör listan av chores till en lista av widgets
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          //Navigerar till skärmen där en ny chore läggs till och väntar på svar
+          final newChoreName = await Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) =>
+                  AddChoreScreen(), //Sidan där användaren lägger till en ny chore
             ),
-          ],
+          );
+
+          if (newChoreName != null && newChoreName.isNotEmpty) {
+            //Kollar så namnet på ny chore inte är tomt
+            Chores newChore = Chores(
+                "", newChoreName, false); //Skapar det nya chores-objektet
+            await myState.apiAddChore(newChore).then(
+                (addedChore) {}); // Lägger till det nya chore-objektet via API
+          }
+          myState.apiFetchChores(); //Uppdaterar chore-listan via API
+        },
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+
+  //Widget för att bygga en Chore
+  Widget buildChoreItem(BuildContext context, Chores chore) {
+    final myState = Provider.of<MyState>(context); //Hämtar instansen av MyState
+    return Padding(
+      padding: const EdgeInsets.all(3.0),
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          border: Border.all(color: Colors.grey, width: 1),
+          borderRadius: BorderRadius.circular(8.0),
+        ),
+        child: ListTile(
+          leading: GestureDetector(
+            onTap: () {
+              myState.toggleChoreStatus(
+                  chore); //Funktionen som ändrar den lokala statusen för chore tille done/ej done
+              myState.apiUpdateChore(
+                  chore); //Uppdaterar status i API så att statusen även sparas på servern
+            },
+            child: chore.done
+                ? Icon(Icons
+                    .check_box) // Om chore är markerad, visa en markerad checkbox
+                : Icon(Icons
+                    .check_box_outline_blank), // Annars, visa en omarkerad checkbox
+          ),
+          title: Text(
+            chore.choreName,
+            style: TextStyle(
+              fontSize: 20,
+              decoration: chore.done
+                  ? TextDecoration.lineThrough
+                  : TextDecoration.none, //Är chore markerad stryks texten
+            ),
+          ),
+          trailing: GestureDetector(
+            onTap: () {
+              myState.apiRemoveChore(
+                  chore); //Funktion som tar bort chore från listan på servern
+            },
+            child: Icon(Icons.close), //Kryss för att ta bort en chore
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
